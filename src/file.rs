3aba4ee1b6264ce28637ce2d729a42228e0a3e85
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2025-03-04 23:27:18 +0200

    MVP Working app but still improvements are needed

--- src/file.rs
+++ src/file.rs
@@ -1,9 +1,7 @@
-use std::fs::read_to_string;
+use std::fs;
 
 pub fn has_local_ips(path: &str, ip_addr: &str) -> bool {
-    let txt: String = read_to_string(path).unwrap();
-
-    for row in txt.lines() {
+    for row in read(path) {
         // If contains ip address and it isn't commented out
         if row.contains(ip_addr) && !row.contains("#") {
             return true;
@@ -13,4 +11,52 @@ pub fn has_local_ips(path: &str, ip_addr: &str) -> bool {
     false
 }
 
-pub fn read_file(path: &str) {}
+pub fn read(path: &str) -> Vec<String> {
+    let txt: String = fs::read_to_string(path).unwrap();
+    // Convert unmutable barrowed &str to actuall mutable strings that arent barrowed
+    txt.lines().map(|s| s.to_string()).collect()
+}
+
+pub fn write(path: &str, contents: Vec<String>) {
+    if let Err(e) = fs::write(path, contents.join("\n")) {
+        eprint!("Failed to write to the file {}", e);
+    }
+}
+
+pub fn comment_ips(path: &str, ip_addr: &str) {
+    // Read hosts file. And receive array of String (that aren't barrowed)
+    let mut hosts = read(path);
+
+    // We use &mut so that we can spawn a mutable line variable
+    for line in &mut hosts {
+        if line.contains(ip_addr) {
+            // * operator is needed to access real mutable variable
+            // * -> line -> hosts[i] then it changes it directly
+            // without it, hosts mutable variable wouldn't be modified
+            *line = format!("# {}", line);
+        }
+    }
+
+    // Write hosts file with new data
+    write(path, hosts);
+    println!("Added comments on ips");
+}
+
+pub fn uncomment_ips(path: &str, ip_addr: &str) {
+    // Read hosts file. And receive array of String (that aren't barrowed)
+    let mut hosts = read(path);
+
+    // We use &mut so that we can spawn a mutable line variable
+    for line in &mut hosts {
+        if line.contains(ip_addr) && line.contains("#") {
+            // * operator is needed to access real mutable variable
+            // * -> line -> hosts[i] then it changes it directly
+            // without it, hosts mutable variable wouldn't be modified
+            *line = line.replace("#", "").trim().to_string();
+        }
+    }
+
+    // Write hosts file with new data
+    write(path, hosts);
+    println!("Removed comments from ip addresses");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-// use std::time::Duration;
-// use tokio::time;
-// use tokio_wifiscanner::scan;
-// use std::fs::OpenOptions;
+use std::time::Duration;
+use tokio::time;
+use tokio_wifiscanner::scan;
 
 // Import modules
 mod file;
@@ -9,51 +8,50 @@ mod file;
 // variables
 const HOSTS_PATH: &str = "hosts";
 const IP_ADDRESS: &str = "192.168.8.109";
+const TARGET_SSID: &str = "Leons iPhone";
 
-// #[tokio::main]
-fn main() {
-    if file::has_local_ips(HOSTS_PATH, IP_ADDRESS) {
-        println!("Has ip addresses")
-    } else {
-        println!("Does not have ip addresses")
+#[tokio::main]
+async fn main() {
+    // Start the monitoring loop
+    // Returns Ok if no error, Err if error occurred
+    loop {
+        match scan().await {
+            Ok(networks) => {
+                // Check if the target SSID is in the list of available networks
+                let connected = networks.iter().any(|network| network.ssid == TARGET_SSID);
+
+                if connected {
+                    // Execute function for the target SSID
+                    on_target_ssid_connected();
+                } else {
+                    // Execute function for other networks
+                    on_other_ssid_connected();
+                }
+            }
+            Err(e) => eprintln!("Failed to scan Wi-Fi networks: {}", e),
+        }
+
+        // Wait for a specified interval before scanning again
+        time::sleep(Duration::from_secs(1)).await; // Adjust the interval as needed
+        println!("Sleeping");
     }
 }
 
-// async fn waitForNetworkChange() {
-//     // Define the target SSID
-//     let target_ssid = "Leons iPhone";
-//
-//     // Start the monitoring loop
-//     loop {
-//         match scan().await {
-//             Ok(networks) => {
-//                 // Check if the target SSID is in the list of available networks
-//                 let connected = networks.iter().any(|network| network.ssid == target_ssid);
-//
-//                 if connected {
-//                     // Execute function for the target SSID
-//                     on_target_ssid_connected();
-//                 } else {
-//                     // Execute function for other networks
-//                     on_other_ssid_connected();
-//                 }
-//             }
-//             Err(e) => eprintln!("Failed to scan Wi-Fi networks: {}", e),
-//         }
-//
-//         // Wait for a specified interval before scanning again
-//         time::sleep(Duration::from_secs(5)).await; // Adjust the interval as needed
-//     }
-// }
-//
-// fn on_target_ssid_connected() {
-//     // Functionality to execute when connected to the target SSID
-//     println!("Connected to the target SSID.");
-//     // Add your code here
-// }
-//
-// fn on_other_ssid_connected() {
-//     // Functionality to execute when connected to a different SSID
-//     println!("Connected to a different SSID.");
-//     // Add your code here
-// }
+// TODO: Check permissions when tryig to write files
+// TODO: add kill signal check, when ctrl + c is pressed to exit the app
+
+fn on_target_ssid_connected() {
+    // Since we are connected to target wifi ssid, we need to uncomment local ip addresses
+    if !file::has_local_ips(HOSTS_PATH, IP_ADDRESS) {
+        // Doesnt have local ips (commented)
+        file::uncomment_ips(HOSTS_PATH, IP_ADDRESS);
+    }
+}
+
+fn on_other_ssid_connected() {
+    // If we aren't connected to target wifi, we need to comment out local ip addresses
+    if file::has_local_ips(HOSTS_PATH, IP_ADDRESS) {
+        // That means we have local ip addresses
+        file::comment_ips(HOSTS_PATH, IP_ADDRESS);
+    }
+}
